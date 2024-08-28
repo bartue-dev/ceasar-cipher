b47378a67f78fde54cf1350db6f975cf1f46f288
Bartue <[bartbatiancila28@gmail.com]>
2024-08-28 14:29:00 +0800

    Add psuedocode before putting the actual code

--- index.rb
+++ index.rb
@@ -3,3 +3,12 @@
 #ceasar cipher => "Hello there",5
 #return "Miqqt ymiwr"
 #which means each character in the string is substituted with another letter down from the alphabet by the number given
+
+#use gets method to be to input a String
+#use gets result as argument for the main method
+#convert each character of the string with their numeric position in the alphabet
+#after converting each character, add the given number to each of the numeric number
+#convert it into string again after adding the given number to each of the numeric number
+#return the final conversion as final result
+
+
