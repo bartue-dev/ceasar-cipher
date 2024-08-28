1211b6a37f82ca2b4350f01de4ae0b7afca3034f
Bartue <[bartbatiancila28@gmail.com]>
2024-08-28 13:57:51 +0800

    Add ruby file. Add structure for the project

---
+++ index.rb
@@ -0,0 +1,5 @@
+#input a string
+#return a new string where the string input characters will be substituted with another letter with the fixed number of positions down the alphabet
+#ceasar cipher => "Hello there",5
+#return "Miqqt ymiwr"
+#which means each character in the string is substituted with another letter down from the alphabet by the number given
